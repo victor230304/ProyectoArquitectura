43452747b0e2d0d741d88888f8ede4fe31a3a881
Victor Sadia Rodríguez <145647088+victor230304@users.noreply.github.com>
2026-03-05 12:06:20 +0100

    .commit

--- taller_coches/app_gestionar_taller/views.py
+++ taller_coches/app_gestionar_taller/views.py
@@ -1,14 +1,24 @@
 from django.http import JsonResponse
-from .models import Cliente
+from .models import Cliente,Coche
 
 
 def lista_clientes(request):
     clientes = list(Cliente.objects.values("id", "nombre", "telefono", "email"))
     return JsonResponse(clientes, safe=False)
 
+def lista_coches(request):
+    coches = list(Coche.objects.values("marca","modelo","matricula"))
+    return JsonResponse(coches,safe=False)
+    
 def detalle_cliente(request, cliente_id):
     try:
         cliente = Cliente.objects.values("id", "nombre", "telefono", "email").get(id=cliente_id)
         return JsonResponse(cliente)
     except Cliente.DoesNotExist:
         return JsonResponse({"error": "Cliente no encontrado"}, status=404)
+def detalle_coche(request, coche_id):
+    try:
+        coche= Coche.objects.values("id","marca","modelo","matricula").get(id=coche_id)
+        return JsonResponse(coche)
+    except Coche.DoesNotExist:
+        return JsonResponse({"error": "Coche no encontrado"}, status=404)
